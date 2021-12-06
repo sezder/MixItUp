4498525c5567327a4a29a87f7434d8effa8277d0
sezder <84882854+sezder@users.noreply.github.com>
2021-12-06 14:48:53 -0700

    troubleshooting backend issue: model at index.js:25 undefined

--- backend/db/migrations/20211206210801-create-bars-cocktails.js
+++ backend/db/migrations/20211206210801-create-bars-cocktails.js
@@ -1,32 +1,34 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Bars_Cocktails', {
+    return queryInterface.createTable("Bars_Cocktails", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       cocktail_id: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: { model: "Cocktails" },
       },
       bar_id: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: { model: "Bars" },
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Bars_Cocktails');
-  }
+    return queryInterface.dropTable("Bars_Cocktails");
+  },
 };
