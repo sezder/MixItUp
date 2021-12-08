95eb0d40bcfa86e51063d778c51789d853e83a35
sezder <84882854+sezder@users.noreply.github.com>
2021-12-07 22:44:10 -0700

    basic navbar styling

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,35 +1,46 @@
 // frontend/src/components/Navigation/index.js
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import LoginFormModal from '../LoginFormModal';
-import './Navigation.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import LoginFormModal from "../LoginFormModal";
+import "./Navigation.css";
 
-function Navigation({ isLoaded }){
-  const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
 
   let sessionLinks;
   if (sessionUser) {
-    sessionLinks = (
-      <ProfileButton user={sessionUser} />
-    );
+    sessionLinks = <ProfileButton user={sessionUser} />;
   } else {
     sessionLinks = (
       <>
         <LoginFormModal />
-        <NavLink to="/signup">Sign Up</NavLink>
+        <NavLink to="/signup" className="text_large">Sign Up</NavLink>
       </>
     );
   }
 
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">Home</NavLink>
+    <nav>
+      <ul>
+        <li>
+          <NavLink className="text_large" exact to="/home">
+            HOME
+          </NavLink>
+         
+        </li>
+        <li>
+          <NavLink to="/cocktails" className="text_large">EXPLORE</NavLink>
+        </li>
+        <li>
+          <NavLink to="/cocktails/new" className="text_large">SUBMIT COCKTAIL</NavLink>
+        </li>
+        <li>
         {isLoaded && sessionLinks}
-      </li>
-    </ul>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
