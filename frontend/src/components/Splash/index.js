a6fb0f3a772eecfa47a4834801042789d2dfccc0
sezder <84882854+sezder@users.noreply.github.com>
2021-12-11 13:32:36 -0700

    video autoplays muted

---
+++ frontend/src/components/Splash/index.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { NavLink, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import "./Splash.css";
+import "../../index.css";
+import NewCocktailReview from "../NewCocktailReview";
+import ShowReviews from "../ShowReviews";
+import { Modal } from "../../context/Modal";
+import LoginForm from "../LoginFormModal/LoginForm.js";
+import BackgroundGif from "./looping_cocktail.mp4";
+
+const Splash = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const user = useSelector((state) => state.session.user);
+  const userId = user?.id;
+
+  let reviewRestriction;
+  if (userId) {
+    reviewRestriction = <NewCocktailReview />;
+  } else {
+    reviewRestriction = (
+      <>
+        <button onClick={() => setShowModal(true)}>
+          Log in to leave a review.
+        </button>
+        {showModal && (
+          <Modal onClose={() => setShowModal(false)}>
+            <LoginForm />
+          </Modal>
+        )}
+      </>
+    );
+  }
+
+  return (
+    <div className="video_container">
+      <video muted autoplay="autoplay" loop>
+        <source src={BackgroundGif} type="video/mp4"/>
+      </video>
+      
+    </div>
+  );
+};
+
+export default Splash;
