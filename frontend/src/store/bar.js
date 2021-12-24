67b8e86421a3124741c51fbdc932e9d9839142ef
sezder <84882854+sezder@users.noreply.github.com>
2021-12-24 13:59:18 -0700

    thunk, action creator, reducer for indivBar

--- frontend/src/store/bar.js
+++ frontend/src/store/bar.js
@@ -1,5 +1,24 @@
 import { csrfFetch } from "./csrf";
 
+// Get one bar by id
+const GET_ONE_BAR = "bars/GET_ONE_BAR";
+
+const loadOneBar = (bar) => ({
+  type: GET_ONE_BAR,
+  bar,
+});
+
+export const loadOneBar = (barId) => {
+  const res = await csrfFetch(`/api/bars/${barId}`);
+
+  if (res.ok) {
+    const bar = await res.json();
+    dispatch(loadOneBar(bar));
+    return bar;
+  }
+};
+
+// Get all bars
 const GET_ALL_BARS = "bars/GET_ALL_BARS";
 
 const loadAllBars = (bars) => ({
@@ -21,6 +40,9 @@ const initialState = {};
 const barReducer = (state = initialState, action) => {
   let newState = {};
   switch (action.type) {
+    case GET_ONE_BAR:
+      newState.bar = action.payload;
+      return { ...state, ...newState };
     case GET_ALL_BARS:
       action.bars.forEach((bar) => {
         newState[bar.id] = bar;
