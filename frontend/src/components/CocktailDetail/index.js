e728aaa67cca7c4dcd2af564b891ea9180e20b2c
sezder <84882854+sezder@users.noreply.github.com>
2022-03-01 22:22:53 -0700

    optionally render stars on cocktail cards

--- frontend/src/components/CocktailDetail/index.js
+++ frontend/src/components/CocktailDetail/index.js
@@ -3,7 +3,14 @@ import StarRatingComponent from "react-star-rating-component";
 import "./CocktailDetail.css";
 import "../../index.css";
 
-const CocktailDetail = ({ id, name, description, imageUrl, avgRating }) => {
+const CocktailDetail = ({
+  id,
+  name,
+  description,
+  imageUrl,
+  avgRating,
+  numReviews,
+}) => {
   return (
     <div className="card">
       <a href={`/cocktails/${id}`} id="card_link">
@@ -13,14 +20,17 @@ const CocktailDetail = ({ id, name, description, imageUrl, avgRating }) => {
 
         <div className="card_content">
           <h2 className="card_title text_large">{name}</h2>
-          <StarRatingComponent
-            name="uneditableRatingAvg"
-            starCount={5}
-            value={avgRating}
-            starColor="#d1c1ae"
-            emptyStarColor="#090C0B"
-            editable={false}
-          />
+          {numReviews > 0 && (
+            <StarRatingComponent
+              name="uneditableRatingAvg"
+              starCount={5}
+              value={avgRating}
+              starColor="#d1c1ae"
+              emptyStarColor="#090C0B"
+              editable={false}
+            />
+          )}
+
           <p className="card_info text_small">{description}</p>
         </div>
       </a>
