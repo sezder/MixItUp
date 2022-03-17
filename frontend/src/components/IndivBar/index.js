c6c53d9ac1834ce083f7503d0d0e56dbd3dd5875
sezder <84882854+sezder@users.noreply.github.com>
2022-03-17 17:36:59 -0600

    fixed state issue with checkins

--- frontend/src/components/IndivBar/index.js
+++ frontend/src/components/IndivBar/index.js
@@ -96,6 +96,7 @@ const IndivBar = ({ setBarComponent, barComponent }) => {
         ) : (
           <br></br>
         )}
+        
         <ul>
           <div>
             <p onClick={() => setBarComponent("info")}>
